3a132c9d30b767422d9130700abebc036d44055e
ncbentley4 <ncbentley4@gmail.com>
2020-05-08 18:01:09 -0600

    Build array to hold board

--- script/script.js
+++ script/script.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
+
+
+
+$(function() {
   console.log('Jquery is running!');
+  // Define my board with an array of arrays
+  const board = []
+  const squares = $('.board__square');
+  for (let i = 0; i < 8; i++) {
+    let row = [];
+    for (let j = 0; j < 8; j++) {
+      row.push(squares[(i * 8) + j]);
+    }
+    board.push(row);
+  }
+  console.log(board);
 });
