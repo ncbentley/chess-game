acbd5e4a176ea4adcd0da34028e6cf31031352ba
ncbentley4 <ncbentley4@gmail.com>
2020-05-11 11:12:23 -0600

    Pawn class + move piece functionality

--- script/script.js
+++ script/script.js
@@ -5,26 +5,36 @@ const board = {
 }
 
 class Piece {
-  constructor(location, color) {
+  constructor(location, color, $icon) {
     this.color = color;
     this.location = location;
-    pieces.push(this);
+    this.$icon = $icon;
+    board.squares[location[0]][location[1]].append($icon);
+    board.pieces.push(this);
   }
 
   move(target) {
-    for (let i = 0; i < board.pieces; i++) {
-      if (board.pieces[i].location === target) {
+    for (let i = 0; i < board.pieces.length; i++) {
+      if (board.pieces[i].location[0] === target[0] && board.pieces[i].location[1] === target[1]) {
         if (board.pieces[i].color === this.color) {
           return false;
         } else {
+          board.pieces[i].$icon.remove();
           board.captured.push(board.pieces.splice(i, 1));
-          // TODO: Remove piece from DOM
         }
       }
     }
     this.location = target;
-    // TODO: Move piece on DOM
+    this.$icon.remove();
+    board.squares[target[0]][target[1]].append(this.$icon);
     return true;
+  }
+}
+
+class Pawn extends Piece {
+  constructor(location, color) {
+    const $icon = $(`<i class="fas fa-chess-pawn ${color}">`)[0];
+    super(location, color, $icon);
   }
 }
 
@@ -39,6 +49,14 @@ $(function() {
       row.push(squares[(i * 8) + j]);
     }
     board.squares.push(row);
+    if (i === 1) {
+      for (let j = 0; j < 8; j++) {
+        new Pawn([i, j], 'black');
+      }
+    } else if (i === 6) {
+      for (let j = 0; j < 8; j++) {
+        new Pawn([i, j], 'white');
+      }
+    }
   }
-  console.log(board);
 });
